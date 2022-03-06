b3e89b72c39490c0dd4f3ca68a91b3acfa581c4e
Jesse Ochs <jessescottochs@gmail.com>
2022-03-06 15:01:15 -0700

    Method exercises tests pass

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -7,11 +7,20 @@
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
 
+def ascii_translator(number)
+    number.chr
+end
+
+
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
+
+def common_sports(current, favorite)
+current & favorite
+end
 
 
 # method name: #alphabetical_list
@@ -19,10 +28,18 @@
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
 
+def alphabetical_list(games)
+    games.sort.uniq
+end
+
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
+
+def lucky_number(number = 7)
+    "Today's lucky number is #{number}"
+end
 
 
 # method name: #ascii_code
@@ -30,6 +47,14 @@
 # return value: the character's ordinal number
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
+
+def ascii_code(letter)
+    if letter.length == 1 
+        letter.ord 
+    else
+        "Input Error" 
+    end
+end
 
 
 # method name: #pet_pun
@@ -39,6 +64,17 @@
 # console output: if animal is 'dog', 'Dogs are paw-some!' (awesome)
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
+def pet_pun(pet)
+    if pet == 'cat'
+       puts "Cats are purr-fect!"
+    elsif pet == 'dog'
+       puts "Dogs are paw-some!"
+    elsif pet == 'rabbit'
+       puts "I think rabbits have pet-tential!"
+    else
+        nil 
+    end 
+end
 
 
 # method name: #twenty_first_century?
@@ -46,3 +82,6 @@
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
 
+def twenty_first_century?(year)
+    year >= 2000 ? true : false
+end
