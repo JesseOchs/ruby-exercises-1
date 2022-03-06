bb36c14615ed2a41b1ba5a7597b9cee06e9c4eab
Jesse Ochs <jessescottochs@gmail.com>
2022-03-06 15:12:50 -0700

    Debugging exercises complete

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -6,14 +6,14 @@
 # To the top, the first line, where the error occured, and ONLY THEN fix the error
 
 def decrement_smallest_value(nested_array)
-  smallest_value = nested_array.flatten.max
-  nested_array.each do |array|
-    array.each do |current_value|
-      if smallest_value > current_value
-        smallest_value = current_valu
-      end
-    end
-  end
+  smallest_value = nested_array.flatten.min
+  # nested_array.each do |array|
+  #   array.each do |current_value|
+  #     if smallest_value > current_value
+  #       smallest_value = current_valu
+  #     end
+  #   end
+  # end
   smallest_value -= 1
 end
 
@@ -21,14 +21,14 @@ end
 # Don't look at this method prior to running the test
 # Run rspec, let the test fail, and go through the stack trace again
 def increment_greatest_value(nested_array)
-  greatest_value = nested_array.flatten.min
-  nested_array.each do |array|
-    array.each do |current_value|
-      if greatest_value < nil
-        greatest_value = current_value
-      end
-    end
-  end
+  greatest_value = nested_array.flatten.max
+  # nested_array.each do |array|
+  #   array.each do |current_value|
+  #     if greatest_value < nil
+  #       greatest_value = current_value
+  #     end
+  #   end
+  # end
   greatest_value += 1
 end
 
@@ -36,10 +36,11 @@ end
 # Use p and puts in order to find what's wrong with our method
 
 def isogram?(string)
-  original_length = string.length
-  string_array = string.downcase.split
-  unique_length = string_array.uniq.length
-  original_length == unique_length
+  string.length == string.split(//).uniq.length
+  # original_length = string.length
+  # string_array = string.downcase.split
+  # unique_length = string_array.uniq.length
+  # original_length == unique_length
 end
 
 # Can you guess what's next?
@@ -50,7 +51,7 @@ end
 # Once you find the error, fix it and get the test to pass
 
 def yell_greeting(string)
-  name = string
-  name = name.downcase
-  greeting = "WASSAP, #{name}!"
+  # name = string
+  # name = name.downcase
+  greeting = "WASSAP, #{string.upcase}!"
 end
